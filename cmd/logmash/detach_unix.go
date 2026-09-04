beb155f35842124ad6facb9a961ac33a1bf435dd
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 01:19:59 -0700

    Detach webhook-only logmash subscriptions

---
+++ cmd/logmash/detach_unix.go
@@ -0,0 +1,48 @@
+//go:build !windows
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+)
+
+func detach(args []string) (int, string, error) {
+	exe, err := os.Executable()
+	if err != nil {
+		return 0, "", err
+	}
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return 0, "", err
+	}
+	logDir := filepath.Join(cacheDir, "smoke")
+	if err := os.MkdirAll(logDir, 0o700); err != nil {
+		return 0, "", err
+	}
+	logPath := filepath.Join(logDir, "logmash-background.log")
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o600)
+	if err != nil {
+		return 0, "", err
+	}
+	defer logFile.Close()
+	devNull, err := os.OpenFile(os.DevNull, os.O_RDWR, 0)
+	if err != nil {
+		return 0, "", err
+	}
+	defer devNull.Close()
+
+	cmd := exec.Command(exe, args...)
+	cmd.Env = append(os.Environ(), "LOGMASH_DETACHED=1")
+	cmd.Stdin = devNull
+	cmd.Stdout = devNull
+	cmd.Stderr = logFile
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+	if err := cmd.Start(); err != nil {
+		return 0, "", fmt.Errorf("start detached child: %w", err)
+	}
+	return cmd.Process.Pid, logPath, nil
+}
