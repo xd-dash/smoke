b09436fb16a560014daafa5a3988f44479875d81
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 15:58:13 -0700

    Preserve source relationship on stdout

--- callback/stdout.go
+++ callback/stdout.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 type Stdout struct {
@@ -17,6 +18,11 @@ func (s Stdout) Handle(_ context.Context, message Message) error {
 	w := s.Writer
 	if w == nil {
 		w = os.Stdout
+	}
+	if message.Source != "" && message.Channel != "" {
+		source := strings.TrimSuffix(message.Source, ".logma.sh")
+		_, err := fmt.Fprintf(w, "%s:%s\t%s\n", source, message.Channel, message.Payload)
+		return err
 	}
 	_, err := fmt.Fprintln(w, message.Payload)
 	return err
