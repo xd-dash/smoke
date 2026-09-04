30c80f794b74935d60d83c894b2d9518b9eaad61
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 13:49:15 -0700

    Add Unix Logmash session process control

---
+++ session/process_unix.go
@@ -0,0 +1,24 @@
+//go:build !windows
+
+package session
+
+import (
+	"os"
+	"syscall"
+)
+
+func processAlive(pid int) bool {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return false
+	}
+	return process.Signal(syscall.Signal(0)) == nil
+}
+
+func stopProcess(pid int) error {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	return process.Signal(syscall.SIGTERM)
+}
