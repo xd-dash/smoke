0dfe6e0b0f3baac4537e0b0ec4993277452fb9dc
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 13:49:29 -0700

    Add Windows Logmash session process control

---
+++ session/process_windows.go
@@ -0,0 +1,26 @@
+//go:build windows
+
+package session
+
+import (
+	"os"
+
+	"golang.org/x/sys/windows"
+)
+
+func processAlive(pid int) bool {
+	h, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION, false, uint32(pid))
+	if err != nil {
+		return false
+	}
+	_ = windows.CloseHandle(h)
+	return true
+}
+
+func stopProcess(pid int) error {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	return process.Kill()
+}
