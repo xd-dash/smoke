ba272976c8a98c5314423238f1c6872b9c508263
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 19:05:18 -0700

    Keep detached Logmash output on inherited console

--- cmd/logmash/detach_windows.go
+++ cmd/logmash/detach_windows.go
@@ -6,42 +6,22 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 )
 
-func detach(args []string) (int, string, error) {
+func detach(args []string) (int, error) {
 	exe, err := os.Executable()
 	if err != nil {
-		return 0, "", err
+		return 0, err
 	}
-	cacheDir, err := os.UserCacheDir()
-	if err != nil {
-		return 0, "", err
-	}
-	logDir := filepath.Join(cacheDir, "smoke")
-	if err := os.MkdirAll(logDir, 0o700); err != nil {
-		return 0, "", err
-	}
-	logPath := filepath.Join(logDir, "logmash-background.log")
-	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o600)
-	if err != nil {
-		return 0, "", err
-	}
-	defer logFile.Close()
-	devNull, err := os.OpenFile(os.DevNull, os.O_RDWR, 0)
-	if err != nil {
-		return 0, "", err
-	}
-	defer devNull.Close()
 
 	childArgs := append([]string{"logmash"}, args...)
 	cmd := exec.Command(exe, childArgs...)
 	cmd.Env = append(os.Environ(), "LOGMASH_DETACHED=1")
-	cmd.Stdin = devNull
-	cmd.Stdout = devNull
-	cmd.Stderr = logFile
+	cmd.Stdin = nil
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	if err := cmd.Start(); err != nil {
-		return 0, "", fmt.Errorf("start detached child: %w", err)
+		return 0, fmt.Errorf("start detached child: %w", err)
 	}
-	return cmd.Process.Pid, logPath, nil
+	return cmd.Process.Pid, nil
 }
