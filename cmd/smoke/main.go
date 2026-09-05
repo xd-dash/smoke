4e79847f888120fb02709d80cc0509a2e50ba320
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 17:39:54 -0700

    Compile Logmash into Smoke

--- cmd/smoke/main.go
+++ cmd/smoke/main.go
@@ -1,47 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/xd-dash/smoke/command"
+	_ "github.com/xd-dash/smoke/cmd/logmash"
+	"github.com/xd-dash/smoke/smokeapp"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		die("usage: smoke <registered-command> [args ...]")
-	}
-
-	registry, err := command.New(
-		command.Command{
-			Name:      "logmash",
-			GoPackage: "github.com/xd-dash/smoke/cmd/logmash",
-		},
-	)
-	if err != nil {
-		die("registry: %v", err)
-	}
-
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
-	path, err := registry.Resolve(ctx, os.Args[1])
-	if err != nil {
-		die("resolve %s: %v", os.Args[1], err)
-	}
-
-	// On Unix this replaces Smoke with the resolved command. The command owns
-	// its terminal/process lifecycle exactly as though it had been invoked
-	// directly. Smoke has no knowledge of that command's provider grammar.
-	if err := command.Exec(path, os.Args[2:]); err != nil {
-		die("run %s: %v", os.Args[1], err)
-	}
-}
-
-func die(format string, args ...any) {
-	fmt.Fprintf(os.Stderr, "smoke: "+format+"\n", args...)
-	os.Exit(1)
+	smokeapp.Main(os.Args[1:])
 }
