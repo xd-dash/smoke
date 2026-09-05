145565988631f96744b2df80d1efe3bde66f1e24
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 19:25:12 -0700

    Remove stdout detach signaling

--- session/process_windows.go
+++ session/process_windows.go
@@ -3,7 +3,6 @@
 package session
 
 import (
-	"fmt"
 	"os"
 
 	"golang.org/x/sys/windows"
@@ -11,21 +10,13 @@ import (
 
 func processAlive(pid int) bool {
 	h, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION, false, uint32(pid))
-	if err != nil {
-		return false
-	}
+	if err != nil { return false }
 	_ = windows.CloseHandle(h)
 	return true
 }
 
 func stopProcess(pid int) error {
 	process, err := os.FindProcess(pid)
-	if err != nil {
-		return err
-	}
+	if err != nil { return err }
 	return process.Kill()
-}
-
-func detachOutputProcess(pid int) error {
-	return fmt.Errorf("stdout detach signal is not supported on Windows for pid %d", pid)
 }
