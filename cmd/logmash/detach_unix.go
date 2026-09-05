958cdf45a28a6abf6f79cac83f0a362c71c05f0f
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 19:24:42 -0700

    Rename Logmash background marker

--- cmd/logmash/detach_unix.go
+++ cmd/logmash/detach_unix.go
@@ -11,19 +11,14 @@ import (
 
 func detach(args []string) (int, error) {
 	exe, err := os.Executable()
-	if err != nil {
-		return 0, err
-	}
-
+	if err != nil { return 0, err }
 	childArgs := append([]string{"logmash"}, args...)
 	cmd := exec.Command(exe, childArgs...)
-	cmd.Env = append(os.Environ(), "LOGMASH_DETACHED=1")
+	cmd.Env = append(os.Environ(), "LOGMASH_BACKGROUND=1")
 	cmd.Stdin = nil
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
-	if err := cmd.Start(); err != nil {
-		return 0, fmt.Errorf("start detached child: %w", err)
-	}
+	if err := cmd.Start(); err != nil { return 0, fmt.Errorf("start background child: %w", err) }
 	return cmd.Process.Pid, nil
 }
