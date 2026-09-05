018ac13ae3168158ca3a0c2d59596e1e3ed341e5
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-04 19:28:32 -0700

    Name Logmash unattended launcher directly

---
+++ cmd/logmash/background_unix.go
@@ -0,0 +1,29 @@
+//go:build !windows
+
+package logmash
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func startBackground(args []string) (int, error) {
+	exe, err := os.Executable()
+	if err != nil {
+		return 0, err
+	}
+
+	childArgs := append([]string{"logmash"}, args...)
+	cmd := exec.Command(exe, childArgs...)
+	cmd.Env = append(os.Environ(), "LOGMASH_BACKGROUND=1")
+	cmd.Stdin = nil
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+	if err := cmd.Start(); err != nil {
+		return 0, fmt.Errorf("start background child: %w", err)
+	}
+	return cmd.Process.Pid, nil
+}
