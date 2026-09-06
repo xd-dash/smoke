2ac32da3bb6e2a280184eb8cadd4d9b85892a615
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-05 19:02:50 -0700

    Add shared Windows file locks

--- internal/filelock/filelock_windows.go
+++ internal/filelock/filelock_windows.go
@@ -9,11 +9,15 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-func tryLock(file *os.File) (bool, error) {
+func tryLock(file *os.File, mode Mode) (bool, error) {
+	flags := uint32(windows.LOCKFILE_FAIL_IMMEDIATELY)
+	if mode == Exclusive {
+		flags |= windows.LOCKFILE_EXCLUSIVE_LOCK
+	}
 	var overlapped windows.Overlapped
 	err := windows.LockFileEx(
 		windows.Handle(file.Fd()),
-		windows.LOCKFILE_EXCLUSIVE_LOCK|windows.LOCKFILE_FAIL_IMMEDIATELY,
+		flags,
 		0,
 		1,
 		0,
