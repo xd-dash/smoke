2bb94d93f363afc48d87f9d08377e9df9fecf483
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-05 23:43:11 -0700

    Use component self-registration in default Smoke

--- cmd/smoke/main.go
+++ cmd/smoke/main.go
@@ -4,12 +4,9 @@ import (
 	"os"
 
 	_ "github.com/xd-dash/smoke/cmd/logmash"
-	"github.com/xd-dash/smoke/identity"
-	"github.com/xd-dash/smoke/selfbuild"
 	"github.com/xd-dash/smoke/smokeapp"
 )
 
 func main() {
-	identity.SetComponents(selfbuild.DefaultLogmash)
 	smokeapp.Main(os.Args[1:])
 }
