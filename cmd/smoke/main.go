b06e89b520f8f9b8972951f7b4cd16ecd8025bc6
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-05 23:42:01 -0700

    Embed default Smoke composition identity

--- cmd/smoke/main.go
+++ cmd/smoke/main.go
@@ -4,9 +4,12 @@ import (
 	"os"
 
 	_ "github.com/xd-dash/smoke/cmd/logmash"
+	"github.com/xd-dash/smoke/identity"
+	"github.com/xd-dash/smoke/selfbuild"
 	"github.com/xd-dash/smoke/smokeapp"
 )
 
 func main() {
+	identity.SetComponents(selfbuild.DefaultLogmash)
 	smokeapp.Main(os.Args[1:])
 }
