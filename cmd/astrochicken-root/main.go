a9c295e3b39d171c9fbc3c8d8defe982903bd20e
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-07 14:50:50 -0700

    Expose Astrochicken root seeding command

--- cmd/astrochicken-root/main.go
+++ cmd/astrochicken-root/main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "usage: astrochicken-root <destination>")
+	if len(os.Args) != 3 || os.Args[1] != "seed" {
+		fmt.Fprintln(os.Stderr, "usage: astrochicken-root seed <destination>")
 		os.Exit(2)
 	}
-	if err := astrochicken.Materialize(os.Args[1]); err != nil {
+	if err := astrochicken.Seed(os.Args[2]); err != nil {
 		fmt.Fprintf(os.Stderr, "astrochicken-root: %v\n", err)
 		os.Exit(1)
 	}
