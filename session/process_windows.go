eb28436fadd22f4f58b5e8232c59f7b3b3118e95
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-08 15:07:54 -0700

    Format Windows session process helpers

--- session/process_windows.go
+++ session/process_windows.go
@@ -10,13 +10,17 @@ import (
 
 func processAlive(pid int) bool {
 	h, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION, false, uint32(pid))
-	if err != nil { return false }
+	if err != nil {
+		return false
+	}
 	_ = windows.CloseHandle(h)
 	return true
 }
 
 func stopProcess(pid int) error {
 	process, err := os.FindProcess(pid)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	return process.Kill()
 }
