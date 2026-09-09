6bae32200c1bc7e780ef828f31cde46b20e7bda6
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-09-08 18:58:43 -0700

    Add render operation to cfxd dns-txt tool

--- cmd/cfxd-dns-txt/main.go
+++ cmd/cfxd-dns-txt/main.go
@@ -8,12 +8,44 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 || os.Args[1] != "seed" {
-		fmt.Fprintln(os.Stderr, "usage: cfxd-dns-txt seed <destination>")
-		os.Exit(2)
+	if len(os.Args) < 2 {
+		usage()
 	}
-	if err := dnstxt.Seed(os.Args[2]); err != nil {
-		fmt.Fprintf(os.Stderr, "cfxd-dns-txt: %v\n", err)
-		os.Exit(1)
+
+	switch os.Args[1] {
+	case "seed":
+		if len(os.Args) != 3 {
+			usage()
+		}
+		if err := dnstxt.Seed(os.Args[2]); err != nil {
+			fail(err)
+		}
+	case "render":
+		if len(os.Args) != 3 {
+			usage()
+		}
+		data, err := os.ReadFile(os.Args[2])
+		if err != nil {
+			fail(err)
+		}
+		out, err := dnstxt.RenderTerraformVars(data)
+		if err != nil {
+			fail(err)
+		}
+		if _, err := os.Stdout.Write(out); err != nil {
+			fail(err)
+		}
+	default:
+		usage()
 	}
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: cfxd-dns-txt <seed <destination>|render <xdroute-config.json>>")
+	os.Exit(2)
+}
+
+func fail(err error) {
+	fmt.Fprintf(os.Stderr, "cfxd-dns-txt: %v\n", err)
+	os.Exit(1)
 }
